2923fd5082b881193ab3ec8a64fba00cd0f91110
Michael Garvin <SaintIsidoreComputerServices@gmail.com>
2014-11-15 00:08:06 -0500

    Added working internet connectivity monitor, comments

--- weather_led.py
+++ weather_led.py
@@ -41,19 +41,23 @@ r.start(0)
 g.start(0)
 b.start(0)
 
+#This function puts the program to sleep for the smallest value of time between actions (LED cycling, Weather updating, and debug LED cycling)
 def sleep():
     time.sleep(min(abs(time.time() - last_flip - flip_time), abs(time.time() - last_call - update_interval), abs(time.time() - last_indication - indication_time)))
 
+#This function pulls weather data from wunderground.com, stores it, and puts it into the correct array. 
 def load_weather():
+    global err
     try:
         response = urllib2.urlopen('http://api.wunderground.com/api/' + str(APIKEY) + '/alerts/q/' + str(STATE) + '/' + str(CITY) + '.json')
 #        response = urllib2.urlopen('http://127.0.0.1/changeme.json')
 	html = response.read()
 	report = json.loads(html)
-	err = 0
+#	err = 0
     except (urllib2.HTTPError, urllib2.URLError):
-	print ("%I:%M:%S, Having trouble connecting to the internet... Ignoring for now.")
+	print (time.strftime("%I:%M:%S, Having trouble connecting to the internet... Ignoring for now."))
 	err = "web" 
+#	print ("Error Code Changed - Web Exception")
     global a1
     global a2
     global a3
@@ -63,6 +67,7 @@ def load_weather():
     try:
         a1 = report['alerts'][0]['type']
 	print (a1)
+#	err = 0
 	if a1 == 'WIN':
 	    update_interval = 300
 	else:
@@ -71,27 +76,33 @@ def load_weather():
 	a1 = "..."
 	print (a1)
 	update_interval = 300
+	err = "web"
+#	print ("Error Code Changed - Web Exception 2")
     try:
         a2 = report['alerts'][1]['type']
 	print (a2)
+#	err = 0
     except (IndexError, UnboundLocalError):
 	a2 = "..."
 	print (a2)
     try:
         a3 = report['alerts'][2]['type']
 	print (a3)
+#	err = 0
     except (IndexError, UnboundLocalError):
 	a3 = "..."
 	print (a3)
     try:
         a4 = report['alerts'][3]['type']
 	print (a4)
+#	err = 0
     except (IndexError, UnboundLocalError):
 	a4 = "..."
 	print (a4)
     try:
         a5 = report['alerts'][4]['type']
 	print (a5)
+#	err = 0
     except (IndexError, UnboundLocalError):
 	a5 = "..."
 	print (a5)
@@ -102,7 +113,7 @@ def load_weather():
 	    last_flip = time.time() - 1
     except KeyError:
 	pass
-
+#The LED Indicator Color Values and Timings
 LED = { 'TOR': { 'R': 100, 'G': 0, 'B': 0, 'T': 1 }, 'TOW': { 'R': 100, 'G': 0, 'B': 0, 'T': 0 }, 'WRN': { 'R': 100, 'G':50, 'B': 0, 'T': 1 }, 'SEW': { 'R': 100, 'G': 50, 'B': 0, 'T': 0 }, 'WIN': { 'R': 25, 'G': 25, 'B': 100, 'T': 2.3 }, 'FLO': { 'R': 25, 'G': 100, 'B': 10, 'T': 1 }, 'WAT': { 'R': 25, 'G': 100, 'B': 10, 'T': 0 }, 'WND': { 'R': 60, 'G': 60, 'B': 80, 'T': 2.3 }, 'SVR': { 'R': 100, 'G': 50, 'B': 0, 'T': 2.3 }, 'HEA': { 'R': 0, 'G': 0, 'B': 0, 'T': 2.3 }, 'FOG': { 'R': 0, 'G': 0, 'B': 0, 'T': 2.3 }, 'SPE': { 'R': 0, 'G': 0, 'B': 0, 'T': 2.3 }, 'FIR': { 'R': 0, 'G': 0, 'B': 0, 'T': 2.3 }, 'VOL': { 'R': 0, 'G': 0, 'B': 0, 'T': 2.3 }, 'HWW': { 'R': 0, 'G': 0, 'B': 0, 'T': 1 }, 'HUR': { 'R': 1, 'G': 0, 'B': 0, 'T': 2.3 }, 'REC': { 'R': 0, 'G': 0, 'B': 0, 'T': 2.3 }, 'REP': { 'R': 0, 'G': 0, 'B': 0, 'T': 2.3 }, 'PUB': { 'R': 0, 'G': 0, 'B': 0, 'T': 2.3 }, 'ERR': { 'R': 100, 'G': 0, 'B': 0, 'T': 5 }}
 
 #Warning = .5 seconds, Watch = 0 seconds, Advisory + General Info = 2.3
@@ -151,21 +162,30 @@ while True:
 	
 	if time.time() >= last_indication + indication_time:
 	    if err == "web":
-		r.ChangeDutyCycle(50)
+		r.ChangeDutyCycle(100)
 		time.sleep(1)
 		r.ChangeDutyCycle(0)
+		indication_time = 5
 		last_indication = time.time()
+		if a1 != "...":
+		    err = 0
 		sleep()
 	    if err == "sys":
 		r.ChangeDutyCycle(50)
+		indication_time = 5
 		last_indication == time.time()
 		sleep()
 	    if a1 == "..." and err == 0:
 	    	    b.ChangeDutyCycle(100)
 	            time.sleep(0.10)
 	            b.ChangeDutyCycle(0)
+		    indication_time = 60
 	            last_indication = time.time()
 	            print (time.strftime("%I:%M:%S, LED flickered normally"))
+#		    print (err)
+#		    print ("^^^ Current Error Code")
+#		    print (last_indication)
+#		    print (time.time())
 	            sleep()
     except:
 	print "Error:", sys.exc_info()[0]
